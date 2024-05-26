21ef3c59c19b968d2867f5bea9360f947b5db901
Chupa Chups <test@123.com>
2024-05-26 20:10:15 +1000

    Config setup

---
+++ cfg-setup/init.sh
@@ -0,0 +1,5 @@
+# https://www.atlassian.com/git/tutorials/dotfiles
+git init --bare $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
